0549f8d186a2fb878d3c6930c1d7dee16792a8ea
Jaygiri07 <145977706+Jaygiri07@users.noreply.github.com>
2023-09-26 13:41:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -26,7 +26,7 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('✪ Jᴏɪɴ ᴀʟʟ ᴄʜᴀɴɴᴇʟs ✪', url=f"https://t.me/{temp.U_NAME}?start=chnl_info")
+                    InlineKeyboardButton('✪ Jᴏɪɴ ᴀʟʟ ᴄʜᴀɴɴᴇʟs ✪', callback_data="chnl_info_grp")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
